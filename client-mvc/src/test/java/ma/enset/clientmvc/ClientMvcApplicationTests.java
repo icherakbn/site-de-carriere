ebaf3159940e054affb72b0589031f804c5cd825
halimabnk <115838084+halimabnk@users.noreply.github.com>
2023-04-01 02:37:27 +0200

    Add files via upload

---
+++ client-mvc/src/test/java/ma/enset/clientmvc/ClientMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.clientmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
