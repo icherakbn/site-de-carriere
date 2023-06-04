3837f4d3ed5a3fd05ca0ae9802d00d3e6b25bf76
halimabnk <115838084+halimabnk@users.noreply.github.com>
2023-06-04 22:02:03 +0200

    Delete client-mvc directory

--- client-mvc/src/main/java/ma/enset/clientmvc/sec/repositories/AppRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.enset.clientmvc.sec.repositories;
-
-import ma.enset.clientmvc.sec.entities.AppRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
-    AppRole findByRoleName(String roleName);
-}
--- client-mvc/src/main/java/ma/enset/clientmvc/sec/repositories/AppUserRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.enset.clientmvc.sec.repositories;
-
-import ma.enset.clientmvc.sec.entities.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser,String> {
-    AppUser findByUsername(String username);
-}
